539971ff5905251bbbf45289bdf319d508ced157
00kaito <bednarz@programmer.net>
2018-08-24 10:58:47 +0200

    hello

--- src/pl/designpatterns/Main.java
+++ src/pl/designpatterns/Main.java
@@ -8,22 +8,6 @@ import pl.designpatterns.weather.WeatherForecast;
 public class Main {
 
     public static void main(String[] args) {
-
-        WeatherForecast weatherForecast = new WeatherForecast(25, 1003);
-        RadioNews radioNews = new RadioNews();
-        InternetNews internetNews = new InternetNews();
-        TvNews tvNews = new TvNews();
-        weatherForecast.registerObserver(radioNews);
-        weatherForecast.registerObserver(internetNews);
-        weatherForecast.registerObserver(tvNews);
-
-        weatherForecast.notifyObservers();
-        weatherForecast.unregisterObserver(tvNews);
-        weatherForecast.unregisterObserver(radioNews);
-        System.out.println("New temperature - notify only internet news:");
-        weatherForecast.changeWeatherInfo(18, 1007);
-
-
-
+        System.out.println("All design patterns are located in separate branches");
     }
 }
